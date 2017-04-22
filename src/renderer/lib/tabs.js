9c7e5e28369b3a66476602e4b69955bf381375a7
acheronfail <acheronfail@gmail.com>
2017-04-23 00:37:42 +1000

    improvements

--- src/renderer/lib/tabs.js
+++ src/renderer/lib/tabs.js
@@ -1,22 +1,29 @@
 import Draggabilly from 'draggabilly';
+import EventEmitter from './events';
 import { el, on, off, clamp } from './utils';
+
+// TODO: curve edges of new tab button
+// TODO: fill width better
+// TODO: don't prevent resize if adding tabs
 
 let instanceId = 0;
 
 const svgBackground = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="topleft" viewBox="0 0 214 29" ><path d="M14.3 0.1L214 0.1 214 29 0 29C0 29 12.2 2.6 13.2 1.1 14.3-0.4 14.3 0.1 14.3 0.1Z"/></symbol><symbol id="topright" viewBox="0 0 214 29"><use xlink:href="#topleft"/></symbol><clipPath id="crop"><rect class="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="50%" height="100%" transfrom="scale(-1, 1)"><use xlink:href="#topleft" width="214" height="29" class="xi-tab-background"/><use xlink:href="#topleft" width="214" height="29" class="xi-tab-shadow"/></svg><g transform="scale(-1, 1)"><svg width="50%" height="100%" x="-100%" y="0"><use xlink:href="#topright" width="214" height="29" class="xi-tab-background"/><use xlink:href="#topright" width="214" height="29" class="xi-tab-shadow"/></svg></g></svg>`;
 const svgNewTab = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><symbol id="addnew" viewBox="0 0 53 29" ><path d="M 14.3 5 L 34.3 5 43 22 22.3 22 Z" /></symbol><symbol id="topright" viewBox="0 0 53 29"><use xlink:href="#addnew"/></symbol><clipPath id="crop"><rect class="mask" width="100%" height="100%" x="0"/></clipPath></defs><svg width="100%" height="100%" transfrom="scale(1, 1)"><use xlink:href="#addnew" width="53" height="29" class="xi-tab-background"/><use xlink:href="#addnew" width="53" height="29" class="xi-tab-shadow"/></svg></svg>`;
 
-export default class Tabs {
+export default class Tabs extends EventEmitter {
   constructor(workspace) {
+    super();
+
     this.workspace = workspace;
     this.draggabillyInstances = [];
     this.id = instanceId++;
 
-    // TODO: Option
+    // TODO: Options
     this.tabOverlapDistance = 14;
     this.minWidth = 45;
     this.maxWidth = 243;
-    // TODO: Option
+    // TODO: Options
 
     const newBtnBg = el('div', null, 'xi-tab-background');
     newBtnBg.innerHTML = svgNewTab;
@@ -27,41 +34,14 @@ export default class Tabs {
     this.contentEl = el('div', [this.newTabButton], 'xi-tabs-content');
     this.bottomBar = el('div', null, 'xi-tabs-bottom-bar');
 
-    this.el = el('div', [this.contentEl], 'xi-tabs-container');
+    this.el = el('div', [this.contentEl, this.bottomBar], 'xi-tabs-container');
     this.el.setAttribute('data-xi-tabs-instance-id', this.id);
-    this.container = workspace.el.appendChild(el('div', [this.el, this.styleEl, this.bottomBar], 'xi-tabs'));
+    this.container = workspace.el.appendChild(el('div', [this.el, this.styleEl], 'xi-tabs'));
 
     this.registerEvents();
     this.layoutTabs();
     this.fixZIndices();
     this.registerDragEvents();
-
-    // Connect events to workspace...
-    this.on('tabAdd', (e) => {/* ... */});
-    this.on('tabRemove', (e) => {
-      const tab = e.detail.tab;
-      this.workspace.closeView(tab.dataset.id);
-    });
-    this.on('activeTabChange', (e) => {
-      const tab = e.detail.tab;
-      this.workspace.selectView(tab.dataset.id);
-    });
-  }
-
-  /**
-   * Events (uses DOM events).
-   */
-
-  on(event, f) {
-    on(this.el, event, f, false);
-  }
-
-  off(event, f) {
-    off(this.el, event, f, false);
-  }
-
-  emit(event, data) {
-    this.el.dispatchEvent(new CustomEvent(event, { detail: data }));
   }
 
   registerEvents() {
@@ -69,7 +49,7 @@ export default class Tabs {
 
     on(this.el, 'click', ({ target }) => {
       if (target.classList.contains('xi-new-tab')) {
-        this.addTab(); // TODO: opts
+        this.emit('new');
       }
       if (target.classList.contains('xi-tab-close')) {
         this.removeTab(target.parentNode); // TODO: before close
@@ -79,11 +59,38 @@ export default class Tabs {
     on(this.el, 'mousedown', ({ target }) => {
       if (target.classList.contains('xi-new-tab')) return;
       if (target.classList.contains('xi-tab') ||
-          target.classList.contains('xi-tab-content') ||
-          target.classList.contains('xi-tab-icon')) {
-        this.setCurrentTab(target);
+          target.classList.contains('xi-tab-icon') ||
+          target.classList.contains('xi-tab-content')) {
+        this.selectTab(target);
       }
     }, false);
+
+    // Prevent resizing while mouse is over the element.
+    const checkOutside = ({ pageX: x, pageY: y }) => {
+      if (!this.container.contains(document.elementFromPoint(x, y))) {
+        enableResize();
+      }
+    };
+
+    const disableResize = () => {
+      this._cachedTabWidth = this.tabWidth();
+      this.el.classList.add('xi-tabs-no-resize');
+      on(window, 'mousemove', checkOutside, false);
+    };
+
+    const enableResize = () => {
+      off(window, 'mousemove', checkOutside, false);
+      this.el.classList.remove('xi-tabs-no-resize');
+      this.el.classList.add('xi-tabs-animate');
+      this.layoutTabs();
+      setTimeout(() => this.el.classList.remove('xi-tabs-animate'), 200);
+    };
+
+    on(this.el, 'mousemove', (e) => {
+      if (!this.el.classList.contains('xi-tabs-no-resize')) {
+        disableResize();
+      }
+    });
   }
 
   /**
@@ -95,8 +102,11 @@ export default class Tabs {
   }
 
   tabWidth() {
+    if (this.el.classList.contains('xi-tabs-no-resize')) {
+      return this._cachedTabWidth;
+    }
     const tabsContentWidth = this.contentEl.clientWidth - this.tabOverlapDistance;
-    const width = (tabsContentWidth / this.tabs.length) + this.tabOverlapDistance;
+    const width = (tabsContentWidth / this.tabs().length) + this.tabOverlapDistance;
     return clamp(width, this.minWidth, this.maxWidth);
   }
 
@@ -122,14 +132,20 @@ export default class Tabs {
 
     this.cleanUpPreviouslyDraggedTabs();
     for (const tab of tabs) tab.style.width = `${tabWidth}px`;
-    if (tabs.length > 0) tabs[tabs.length - 1].style.width = '48px';
+
+    if (tabs.length == 1) {
+      this.newTabButton.style.display = 'none';
+    }
+    else if (tabs.length > 1) {
+      this.newTabButton.style.display = 'block';
+      this.newTabButton.style.width = '48px';
+    }
 
     requestAnimationFrame(() => {
       let styleHTML = '';
       this.tabPositions().forEach((left, i) => {
-        // [data-xi-tabs-instance-id=${this.id}]
         styleHTML += `
-          .xi-tabs-content .xi-tab:nth-child(${i+1})  {
+          .xi-tabs-container[data-xi-tabs-instance-id='${this.id}'] .xi-tab:nth-child(${i+1})  {
             transform: translate3d(${left}px, 0, 0);
           }
         `;
@@ -150,7 +166,7 @@ export default class Tabs {
     tabs.forEach((tab, i) => {
       let zIndex = tabs.length - i;
 
-      if (tab.classList.contains('xi-tab-current')) {
+      if (tab.classList.contains('xi-tab-selected')) {
         this.bottomBar.style.zIndex = tabs.length + 1;
         zIndex = tabs.length + 2;
       }
@@ -195,7 +211,7 @@ export default class Tabs {
             tab.classList.remove('xi-tab-currently-dragged');
             this.el.classList.remove('xi-tabs-sorting');
 
-            this.setCurrentTab(tab);
+            this.selectTab(tab);
             tab.classList.add('xi-tab-just-dragged');
 
             requestAnimationFrame(() => {
@@ -230,17 +246,13 @@ export default class Tabs {
     }
   }
 
-  setCurrentTab(tab) {
-    const currentTab = this.el.querySelector('.xi-tab-current');
-    if (currentTab) currentTab.classList.remove('xi-tab-current');
-    tab.classList.add('xi-tab-current');
+  selectTab(tab) {
+    const currentTab = this.el.querySelector('.xi-tab-selected');
+    if (currentTab) currentTab.classList.remove('xi-tab-selected');
+    tab.classList.add('xi-tab-selected');
     this.fixZIndices();
-    this.emit('activeTabChange', { tab });
+    this.emit('change', tab.dataset);
   }
-
-  /**
-   * External methods.
-   */
 
   createTab() {
     const svg = el('div');
@@ -253,38 +265,34 @@ export default class Tabs {
     return el('div', [bg, icon, title, close], 'xi-tab');
   }
 
-  addTab(data) {
-    const tab = this.createTab();
+  removeTab(tab, force) {
+    if (!force && !this.signal('remove', null, tab.dataset)) return null;
 
-    tab.classList.add('xi-tab-just-added');
-    setTimeout(() => tab.classList.remove('xi-tab-just-added'), 500);
-
-    this.contentEl.insertBefore(tab, this.newTabButton);
-    this.updateTab(tab, Object.assign({}, { title: '', icon: '' }, data));
-    this.emit('tabAdd', { tab });
-    this.setCurrentTab(tab);
-    this.layoutTabs();
-    this.fixZIndices();
-    this.registerDragEvents();
-  }
-
-  removeTab(tab) {
-    if (tab.classList.contains('xi-tab-current')) {
-      if (tab.previousElementSibling) {
-        this.setCurrentTab(tab.previousElementSibling);
-      } else if (tab.nextElementSibling) {
-        this.setCurrentTab(tab.nextElementSibling);
+    if (tab.classList.contains('xi-tab-selected')) {
+      if (tab.previousElementSibling && tab.previousElementSibling != this.newTabButton) {
+        this.selectTab(tab.previousElementSibling);
+      } else if (tab.nextElementSibling && tab.nextElementSibling != this.newTabButton) {
+        this.selectTab(tab.nextElementSibling);
       }
     }
-    // TODO: move and then remove - for animation
-    tab.parentNode.removeChild(tab);
-    this.emit('tabRemove', { tab });
-    this.layoutTabs();
+
+    // Animate out and then remove.
+    this.el.classList.add('xi-tabs-animate');
+    tab.classList.add('xi-tab-just-removed');
+    setTimeout(() => {
+      tab.parentNode.removeChild(tab);
+      this.layoutTabs();
+      setTimeout(() => this.el.classList.remove('xi-tabs-animate'), 120);
+    }, 150);
+
+    this.emit('removed', tab.dataset);
     this.fixZIndices();
     this.registerDragEvents();
+    return tab;
   }
 
   updateTab(tab, data) {
+    if (!data.id) throw new Error('Tab must have an `id` property!');
     for (const key in data) {
       if (key == 'title') {
         tab.querySelector('.xi-tab-title').textContent = data.title;
@@ -296,48 +304,39 @@ export default class Tabs {
     }
   }
 
+  /**
+   * External methods.
+   */
 
+  add(data) {
+    if (!data.id) throw new Error('Tab must have an `id` property!');
+    const tab = this.createTab();
 
+    tab.classList.add('xi-tab-just-added');
+    setTimeout(() => tab.classList.remove('xi-tab-just-added'), 500);
 
+    this.contentEl.insertBefore(tab, this.newTabButton);
+    this.updateTab(tab, Object.assign({}, { title: '', icon: '' }, data));
+    this.emit('add', tab.dataset);
+    this.selectTab(tab);
+    this.layoutTabs();
+    this.fixZIndices();
+    this.registerDragEvents();
+  }
 
+  has(id) {
+    const tab = this.el.querySelector(`.xi-tab[data-id='${id}']`);
+    if (tab && tab.classList.contains('xi-tab-just-removed')) return false;
+    return !!tab;
+  }
 
+  select(id) {
+    const tab = this.el.querySelector(`.xi-tab[data-id='${id}']`);
+    if (tab) this.selectTab(tab);
+  }
 
-
-
-
-
-
-
-  // TODO: ensure ordered ?
-  // update(activeView) {
-  //   for (const id in this.workspace.views) {
-  //     const view = this.workspace.views[id];
-
-  //     let tab = this.getTabForView(view);
-  //     if (!tab) {
-  //       tab = this.createTab(view);
-  //     }
-  //     if (view == activeView) {
-  //       tab.classList.add('active');
-  //     } else {
-  //       tab.classList.remove('active');
-  //     }
-  //   }
-  // }
-
-  // getTabForView(view) {
-  //   return this.tabs.querySelector(`[data-view-id=${view.id}]`);
-  // }
-
-  // onclick(e) {
-  //   const tab = e.target;
-  //   this.workspace.selectView(tab.dataset.viewId);
-  // }
-
-  // createTab(view) {
-  //   const tab = el('div', view.id, 'xi-tab');
-  //   tab.setAttribute('data-view-id', view.id);
-  //   on(tab, 'click', this.onclick, false);
-  //   return this.tabs.appendChild(tab);
-  // }
+  remove(id) {
+    const tab = this.el.querySelector(`.xi-tab[data-id='${id}']`);
+    if (tab) this.removeTab(tab, true);
+  }
 }
--- src/renderer/lib/workspace.js
+++ src/renderer/lib/workspace.js
@@ -13,6 +13,12 @@ export default class Workspace {
     this.el = place.appendChild(el('div', null, 'xi-workspace'));
 
     this.tabs = new Tabs(this);
+    this.tabs.on('new', () => this.newView());
+    this.tabs.on('change', ({ id }) => this.selectView(id));
+    this.tabs.on('remove', (e, { id }) => {
+      // TODO: prevent if isDirty from core.
+      if (!this.closeView(id)) e.preventDefault();
+    });
 
     this.core = cp.spawn(CORE_PATH);
     this.core.stdout.on('data', this.receiveFromCore.bind(this));
@@ -31,15 +37,15 @@ export default class Workspace {
     });
   }
 
-  getActiveView() {
-    return this.views[this._selected_view];
+  activeViewId() {
+    return this._selected_view;
   }
 
   selectView(id) {
-    const view = this.views[id];
-    if (view && id != this._selected_view) {
+    if (this.views[id] && id != this._selected_view) {
       this._selected_view = id;
-      view.show();
+      this.views[id].show();
+      this.tabs.select(id);
 
       for (const key in this.views) {
         if (id != key) this.views[key].hide();
@@ -50,27 +56,36 @@ export default class Workspace {
   createView(id) {
     const view = new View(this, id);
     this.views[id] = view;
+    this.tabs.add({ title: view.id, id: view.id });
     this.selectView(id);
-    this.tabs.addTab({ title: view.id, id: view.id });
   }
 
+  // return true when closing, false otherwise.
+  // TODO: query core to see if view is dirty.
   closeView(id) {
-    const view = this.views[id];
-    if (view) {
-      // TODO:
-      // view.destroy();
-      view.el.remove();
+    if (this.views[id] /* && !this.isDirty(id) */ ) {
+      this.views[id].destroy();
+      delete this.views[id];
+      return true;
     }
+
+    // TODO: open dialog to user.
+    return false;
   }
 
   /**
-   * Messaging to/from main process.
+   * Messaging to workspace.
+   * Usually from main process. (TODO: May also be from plugins?)
    */
 
   message(method, ...args) {
-    if (method == 'new-window') {} // TODO:
     if (method == 'new-file') {
       this.newView();
+    } else if (method == 'close-file') {
+      this.tabs.remove(this.activeViewId());
+    } else {
+      console.warn('message not handled from main process');
+      console.log(method, args);
     }
   }
 
