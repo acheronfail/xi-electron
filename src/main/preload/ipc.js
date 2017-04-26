5dfded0d99998123ffabfe35b02e171f98b92b8f
acheronfail <acheronfail@gmail.com>
2017-04-26 16:22:00 +1000

    window state now saves

--- src/main/preload/ipc.js
+++ src/main/preload/ipc.js
@@ -1,6 +1,27 @@
 import { ipcMain } from 'electron';
-import { showErrorBox } from './dialog';
+import { openFile, openFolder, showErrorBox } from '../dialog';
+
+export function init() {
+  // ...
+}
 
 ipcMain.on('dialog-error', (e, ...args) => {
   showErrorBox(...args);
 });
+
+ipcMain.on('dialog-open-file', (e, win, opts) => {
+  openFile(win || w(e), opts);
+});
+
+ipcMain.on('dialog-open-folder', (e, win, opts) => {
+  openFolder(win || w(e), opts);
+});
+
+/**
+ * Helpers.
+ */
+
+// Gets the BrowserWindow from an event.
+function w(e) {
+  return BrowserWindow.fromWebContents(e.sender);
+}
