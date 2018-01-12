d5c3073afd09a823fc142adbdf3296a55eb5c032
acheronfail <acheronfail@gmail.com>
2018-01-12 11:27:21 +1100

    add label to signify which view type is currently being used (for debugging)

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -2,12 +2,13 @@ import Workspace from './workspace';
 import { DEVMODE } from '../utils/environment';
 import { ViewType } from './view/index';
 
-const workspace = new Workspace(document.body, {
-  viewType: ViewType.DOM
-});
+const opts = { viewType: ViewType.DOM };
+const workspace = new Workspace(document.body, opts);
 
 if (DEVMODE) {
   // tslint:disable-next-line
   (<any>window).Core = require('./core').default;
   (<any>window).workspace = workspace;
+  // Show current view in body.
+  document.body.appendChild(document.createTextNode(`ViewType: ${opts.viewType}`));
 }
