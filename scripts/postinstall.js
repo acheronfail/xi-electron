264c8af82f4530da25e21c600a5a866f0608ae87
acheronfail <acheronfail@gmail.com>
2018-02-04 13:27:35 +1100

    removed dirutils dep fixes #10

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,9 +5,6 @@ const fse = require('fs-extra');
 const path = require('path');
 const which = require('which');
 
-// Adds unix-like `pushdir` and `popdir` to the process object.
-require("dirutils");
-
 const PROJECT_ROOT = path.resolve(__dirname, '..');
 const XI_CORE = path.join(PROJECT_ROOT, 'xi-editor', 'rust');
 const XI_CORE_BUILD = path.join(XI_CORE, 'target', 'release', 'xi-core');
@@ -17,12 +14,14 @@ const XI_CORE_DEST = path.join(PROJECT_ROOT, 'src', 'xi', 'xi-core');
 which.sync('cargo');
 
 // Build xi-core.
-process.pushdir(XI_CORE);
-let res = cp.spawnSync('cargo', ['build', '--release'], { encoding: 'utf8', stdio: 'inherit' });
+let res = cp.spawnSync('cargo', ['build', '--release'], {
+  encoding: 'utf8',
+  stdio: 'inherit',
+  cwd: XI_CORE
+});
 if (res.status != 0) {
   throw result.error || new Error('Could not build xi-core!');
 }
-process.popdir();
 
 // Copy build into project source.
 fse.removeSync(XI_CORE_DEST);
@@ -44,12 +43,14 @@ const XI_PLUGINS_SYNTECT_DEST = path.join(XI_PLUGINS_DEST, 'syntect', 'bin', 'xi
 // Build plugins that need building.
 console.log('Building plugins...');
 
-process.pushdir(XI_PLUGINS_SYNTECT);
-res = cp.spawnSync('cargo', ['build', '--release'], { encoding: 'utf8', stdio: 'inherit' });
+res = cp.spawnSync('cargo', ['build', '--release'], {
+  encoding: 'utf8',
+  stdio: 'inherit',
+  cwd: XI_PLUGINS_SYNTECT
+});
 if (res.status != 0) {
   throw result.error || new Error('Could not build syntect-plugin!');
 }
-process.popdir();
 
 // Copy over plugins.
 fse.removeSync(XI_PLUGINS_DEST);
