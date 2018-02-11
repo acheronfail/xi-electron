78894d1abef05083b10b19cc8dfd201473127d1b
acheronfail <acheronfail@gmail.com>
2018-02-11 22:05:00 +1100

    updated script for windows platforms

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,10 +7,11 @@ const fse = require('fs-extra');
 const path = require('path');
 const which = require('which');
 
+const WIN = process.platform == 'win32';
 const PROJECT_ROOT = path.resolve(__dirname, '..');
 const XI_CORE = path.join(PROJECT_ROOT, 'xi-editor', 'rust');
-const XI_CORE_BUILD = path.join(XI_CORE, 'target', 'release', 'xi-core');
-const XI_CORE_DEST = path.join(PROJECT_ROOT, 'src', 'xi', 'xi-core');
+const XI_CORE_BUILD = path.join(XI_CORE, 'target', 'release', WIN ? 'xi-core.exe' : 'xi-core');
+const XI_CORE_DEST = path.join(PROJECT_ROOT, 'src', 'xi', WIN ? 'xi-core.exe' : 'xi-core');
 
 // Do we have cargo & git? (this will throw if not).
 which.sync('git');
@@ -42,8 +43,8 @@ const XI_PLUGINS_PY = path.join(PROJECT_ROOT, 'xi-editor', 'python');
 const XI_PLUGINS_SYNTECT = path.join(XI_CORE, 'syntect-plugin');
 const XI_PLUGINS_SYNTECT_MANIFEST = path.join(XI_PLUGINS_SYNTECT, 'manifest.toml');
 const XI_PLUGINS_SYNTECT_MANIFEST_DEST = path.join(XI_PLUGINS_DEST, 'syntect', 'manifest.toml');
-const XI_PLUGINS_SYNTECT_BUILD = path.join(XI_PLUGINS_SYNTECT, 'target', 'release', 'xi-syntect-plugin');
-const XI_PLUGINS_SYNTECT_DEST = path.join(XI_PLUGINS_DEST, 'syntect', 'bin', 'xi-syntect-plugin');
+const XI_PLUGINS_SYNTECT_BUILD = path.join(XI_PLUGINS_SYNTECT, 'target', 'release', WIN ? 'xi-syntect-plugin.exe' : 'xi-syntect-plugin');
+const XI_PLUGINS_SYNTECT_DEST = path.join(XI_PLUGINS_DEST, 'syntect', 'bin', WIN ? 'xi-syntect-plugin.exe' : 'xi-syntect-plugin');
 
 // Build plugins that need building.
 console.log('Building plugins...');
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 // environment
 export const DEVMODE = /node_modules[\\/]electron[\\/]/.test(process.execPath);
 export const MACOS = process.platform === 'darwin';
+export const WIN = process.platform === 'win32';
 
 // project dirs
 export const PROJECT_ROOT = path.resolve(__dirname, '..', '..');
@@ -10,4 +11,4 @@ export const SOURCE_DIR = path.join(PROJECT_ROOT, 'src');
 
 // xi-core
 export const XI_CORE_DIR = path.join(SOURCE_DIR, 'xi');
-export const XI_CORE_BIN = path.join(XI_CORE_DIR, 'xi-core');
+export const XI_CORE_BIN = path.join(XI_CORE_DIR, WIN ? 'xi-core.exe' : 'xi-core');
