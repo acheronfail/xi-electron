443b720cdadb34c36dc495123a5e61e4f6514ae1
acheronfail <acheronfail@gmail.com>
2018-02-11 15:34:02 +1100

    minor tweaks to DOM gutter

--- src/renderer/view/dom/index.ts
+++ src/renderer/view/dom/index.ts
@@ -1,3 +1,4 @@
+import { nDigits } from '../../../utils/misc';
 import { elt, removeChildren, on, off } from '../../../utils/dom';
 import { StyleSpan, N_RESERVED_STYLES, COLORS } from '../../style-map';
 
@@ -10,6 +11,9 @@ import FontMetrics from './font-metrics';
  * A view built using the Document Object Model.
  * The scrolling mechanism takes inspiration from CodeMirror.
  * See http://marijnhaverbeke.nl/blog/a-pathological-scrolling-model.html
+ *
+ * See the Canvas implementation of a view for commented/documented code of what
+ * how view should be implemeneted.
  */
 export default class DOMView implements View {
 
@@ -22,13 +26,26 @@ export default class DOMView implements View {
   private metrics: FontMetrics;
   private lineCache: LineCache;
 
-  constructor(private controller: ViewController, _opts: ViewOptions) {
+  private drawGutter: boolean = true;
+  private gutterChars: number = 0;
+  private gutterWidth: number = 0;
+  private editorPadding: [number, number] = [5, 0];
+  private gutterPadding: [number, number] = [20, 0];
+  private scrollPastEnd: boolean = false;
+
+  constructor(private controller: ViewController, opts: ViewOptions) {
     this.wrapper = controller.wrapper;
     this.lineCache = controller.lineCache;
 
     this.metrics = new FontMetrics(this.wrapper, {
       family: 'monospace',
       size: 20
+    });
+
+    ['drawGutter', 'gutterPadding', 'editorPadding', 'scrollPastEnd'].forEach((key) => {
+      if (opts[key]) {
+        this[key] = opts[key];
+      }
     });
 
     this.wrapper.style.background = COLORS.BACKGROUND;
@@ -38,17 +55,25 @@ export default class DOMView implements View {
       min-height: 1px;
       cursor: text;
     `);
+    this.gutter = elt('div', null, 'gutter', `
+      position: absolute; left: 0; top: 0;
+      min-height: 100%;
+      z-index: 3;
+      background: #242424;
+      text-align: right;
+      /* padding: ${this.gutterPadding[1]}px ${this.gutterPadding[0]}px; */
+    `);
     this.mover = elt('div', [this.lines], 'mover', 'position: relative');
     this.sizer = elt('div', [this.mover], 'sizer', `
       position: relative;
       box-sizing: content-box;
       border-right: 30px solid transparent;
     `);
-    this.scroller = controller.wrapper.appendChild(elt('div', [this.sizer], 'scroller', `
+    this.scroller = controller.wrapper.appendChild(elt('div', [this.gutter, this.sizer], 'scroller', `
       box-sizing: content-box;
       overflow: scroll !important;
       /* 30px is the magic margin used to hide the element's real scrollbars */
-      /* See overflow: hidden in .CodeMirror */
+      /* Needs overflow: hidden on the wrapper */
       margin-bottom: -30px; margin-right: -30px;
       padding-bottom: 30px;
       position: relative;
@@ -62,7 +87,19 @@ export default class DOMView implements View {
    * Private API
    */
 
+  private measureGutter() {
+    if (this.drawGutter) {
+      const asciiWidth = this.metrics.asciiWidth();
+      this.gutterChars = nDigits(this.lineCache.height());
+      this.gutterWidth = this.gutterChars * asciiWidth + this.gutterPadding[0];
+    } else {
+      this.gutterWidth = 0;
+    }
+    this.gutter.style.width = `${this.gutterWidth}px`;
+  }
+
   private updateViewport(): void {
+    this.measureGutter();
     this.lines.style.marginBottom = `${this.scroller.clientHeight}px`;
     this.sizer.style.height = `${this.calculateTotalHeight()}px`;
   }
@@ -76,10 +113,11 @@ export default class DOMView implements View {
       this.controller.updateViewport();
 
       // HACK: a shameless hack in order to stop an infinite loop from
-      // occurring. When `line.lines` has a margin applied to it (for it to
+      // occurring. When `this.lines` has a margin applied to it (for it to
       // scroll past the end) it triggers a scroll event on the scroller (since
       // its contents changes size) thereby calling this listener again ...
       // and so on ... and so on ...
+      // There's definitely a better way to get around this!
       off(this.scroller, 'scroll', this.onScroll, { capture: false, passive: true });
       this.render();
       // Wait for re-draw to complete, then re-attach scroll listener.
@@ -124,18 +162,31 @@ export default class DOMView implements View {
   }
 
   public render(): void {
-    // Empty container.
+    // Empty containers.
     removeChildren(this.lines);
+    removeChildren(this.gutter);
 
     let { lineStart, lineEnd } = this.getViewport();
     this.lineCache.computeMissing(lineStart, lineEnd);
 
     // Push mover forward as we scroll.
-    this.mover.style.top = `${lineStart * this.metrics.lineHeight()}px`;
+    const height = `${lineStart * this.metrics.lineHeight()}px`;
+    this.mover.style.top = this.gutter.style.top = height;
+    this.mover.style.left = `${this.gutterWidth}px`;
 
     for (let i = lineStart; i < lineEnd + 1; ++i) {
       const line = this.lineCache.get(i);
       if (!line) { continue; }
+
+      // TODO: CodeMirror draws the gutter itself and the line numbers separately.
+      // The gutter itself is placed in the scroller, but the numbers are positioned
+      // within CodeMirror-code (aka the lines container).
+      this.gutter.appendChild(elt('div', `${i}`, null, `
+        color: #5a5a5a;
+        white-space: normal;
+        height: 100%;
+        font: ${this.metrics.fontString()};
+      `));
 
       const lineNode = new LineNode(line, this.metrics);
       this.lines.appendChild(lineNode.element);
@@ -155,14 +206,16 @@ class LineNode {
   constructor(public line: Line, private metrics: FontMetrics) {
     this.asciiWidth = this.metrics.asciiWidth();
     this.lineHeight = this.metrics.lineHeight();
-    this.element = (<HTMLPreElement>elt('pre', null, null, `position: relative;
-                                                            border-width: 0;
-                                                            border-radius: 0;
-                                                            word-wrap: normal;
-                                                            margin: 0;
-                                                            z-index: 2;
-                                                            font-variant-ligatures: contextual;
-                                                            font: ${this.metrics.fontString()}`));
+    this.element = (<HTMLPreElement>elt('pre', null, null, `
+      position: relative;
+      border-width: 0;
+      border-radius: 0;
+      word-wrap: normal;
+      margin: 0;
+      z-index: 2;
+      font-variant-ligatures: contextual;
+      font: ${this.metrics.fontString()};
+    `));
 
     // Build DOM cursors.
     line.cursors.forEach((char) => {
