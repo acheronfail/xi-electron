1aa5ee274b3750c3b06589b0818eeddff56669a9
acheronfail <acheronfail@gmail.com>
2018-07-17 22:55:12 +1000

    update submodule

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -10,7 +10,7 @@ const which = require('which');
 const WIN = process.platform == 'win32';
 const PROJECT_ROOT = path.resolve(__dirname, '..');
 const XI_CORE = path.join(PROJECT_ROOT, 'xi-editor', 'rust');
-const XI_CORE_BUILD = path.join(XI_CORE, 'target', 'release', WIN ? 'xi-core.exe' : 'xi-core');
+const XI_CORE_BUILD = path.join(XI_CORE, 'target', 'debug', WIN ? 'xi-core.exe' : 'xi-core');
 const XI_CORE_DEST = path.join(PROJECT_ROOT, 'src', 'xi', WIN ? 'xi-core.exe' : 'xi-core');
 
 // Do we have cargo & git? (this will throw if not).
@@ -24,7 +24,7 @@ const args = (o) => Object.assign({ encoding: 'utf8', stdio: 'inherit' }, o || {
 res = cp.spawnSync('git', ['submodule', 'update', '--init'], args());
 
 // Build xi-core.
-res = cp.spawnSync('cargo', ['build', '--release'], args({ cwd: XI_CORE }));
+res = cp.spawnSync('cargo', ['build'], args({ cwd: XI_CORE }));
 if (res.status != 0) {
   throw res.error || new Error('Could not build xi-core!');
 }
@@ -43,13 +43,13 @@ const XI_PLUGINS_PY = path.join(PROJECT_ROOT, 'xi-editor', 'python');
 const XI_PLUGINS_SYNTECT = path.join(XI_CORE, 'syntect-plugin');
 const XI_PLUGINS_SYNTECT_MANIFEST = path.join(XI_PLUGINS_SYNTECT, 'manifest.toml');
 const XI_PLUGINS_SYNTECT_MANIFEST_DEST = path.join(XI_PLUGINS_DEST, 'syntect', 'manifest.toml');
-const XI_PLUGINS_SYNTECT_BUILD = path.join(XI_PLUGINS_SYNTECT, 'target', 'release', WIN ? 'xi-syntect-plugin.exe' : 'xi-syntect-plugin');
+const XI_PLUGINS_SYNTECT_BUILD = path.join(XI_CORE, 'target', 'debug', WIN ? 'xi-syntect-plugin.exe' : 'xi-syntect-plugin');
 const XI_PLUGINS_SYNTECT_DEST = path.join(XI_PLUGINS_DEST, 'syntect', 'bin', WIN ? 'xi-syntect-plugin.exe' : 'xi-syntect-plugin');
 
 // Build plugins that need building.
 console.log('Building plugins...');
 
-res = cp.spawnSync('cargo', ['build', '--release'], args({ cwd: XI_PLUGINS_SYNTECT }));
+res = cp.spawnSync('cargo', ['build'], args({ cwd: XI_PLUGINS_SYNTECT }));
 if (res.status != 0) {
   throw res.error || new Error('Could not build syntect-plugin!');
 }
