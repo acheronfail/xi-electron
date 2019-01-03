4931d4705b8e83775f6849770c614375b3b7215b
acheronfail <acheronfail@gmail.com>
2019-01-03 21:18:52 +1100

    fix: minor travis and forge config changes

---
+++ forge.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  make_targets: {
+    win32: ['squirrel'],
+    darwin: ['zip', 'dmg'],
+    linux: ['deb', 'rpm', 'flatpak']
+  },
+  electronPackagerConfig: {
+    packageManager: 'yarn',
+    appBundleId: 'io.acheronfail.xi-electron',
+    ignore: [/\/xi-editor\//]
+  },
+  electronWinstallerConfig: {
+    name: 'xi_electron'
+  },
+  electronInstallerDebian: {},
+  electronInstallerRedhat: {},
+  github_repository: {
+    owner: 'acheronfail',
+    name: 'xi-electron'
+  },
+  windowsStoreConfig: {
+    packageName: 'io.acheronfail.xi-electron',
+    name: 'xi_electron'
+  }
+}
